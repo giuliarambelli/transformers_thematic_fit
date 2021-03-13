e290cc61fdded5a9536f93f975a96230943a56c7
ppedin <67503284+ppedin@users.noreply.github.com>
2021-03-13 20:34:12 +0100

    Update main.py

--- ttf/main.py
+++ ttf/main.py
@@ -18,17 +18,15 @@ models_dict = {'bertbase': 'bert-base-cased', 'bertlarge':'bert-large-cased', 'r
 
 def _run_transformers_mlm(args):
     data_sequences_path = args.input_file_data_sequences  #  dove trovare i dati
-    tokenized_sentences_path = args.input_file_tokenized_sentences  #  dove trovare le frasi tokenizzate
     outdir = outils.check_dir(args.output_dir)  #  output location
     synrel = args.rel
-    sets = args.settings
     models = [models_dict[m] for m in args.model]
     name = args.name
 
     if sorted(sets)[0]=='all':  #  sorted function to list of settings. If lists starts with all, execute for all settings
         sets = ['standard', 'head', 'context', 'control']
     if os.path.isfile(data_sequences_path) and os.path.isfile(tokenized_sentences_path):
-        transformers_mlm_model.build_model(data_sequences_path, tokenized_sentences_path, synrel, outdir, sets, models,name)  #  don't check whether the input files exist
+        transformers_mlm_model.build_model(data_sequences_path, synrel, outdir, models,name)  #  don't check whether the input files exist
     else:
         logger.info('Input path {} or {} does not exist'.format(data_sequences_path, tokenized_sentences_path))
 
@@ -45,12 +43,8 @@ def main():
     parser_transformers_mlm.add_argument('-ids', '--input-file-data-sequences', required=True,
                                          help='path to data (human scores, sentences selected for each'
                                               ' sequence of the dataset')
-    parser_transformers_mlm.add_argument('-its', '--input-file-tokenized-sentences', required=True,
-                                         help='path to file with tokenization of selected sentences')  #  NO NEED FOR IT IN OUR NEW VERSION
-    parser_transformers_mlm.add_argument('-r', '--rel', required=True, choices=['sbj', 'obj'],
-                                         help='thematic role')  #  THE NUMBER OF CHOICES MUST BE AUGMENTED - A COSA SERVONO?  
-    parser_transformers_mlm.add_argument('-s', '--settings', choices=['standard', 'head', 'context', 'control', 'all'], 
-                                         default=['standard'], nargs='+', help='masked settings')
+    parser_transformers_mlm.add_argument('-r', '--rel', required=True, choices=['sbj', 'obj', 'instrument', 'location', 'recipient', 'time'],
+                                         help='thematic role')
     parser_transformers_mlm.add_argument('-m','--model', choices=['bert-base', 'bert-large', 'roberta', 'gpt2'],
                                          nargs='+', default=['bert-large'], help='transformer models')
     parser_transformers_mlm.add_argument('-n', '--name', required=True, help='dataset name')  #  VERIFICARE LA SUA FUNZIONE ANCHE IN RELAZIONE A THEMATIC ROLE
