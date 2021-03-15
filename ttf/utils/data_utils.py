67d9c64bc9e9b88e9e733d8e7a3d5974f7081d23
ppedin <67503284+ppedin@users.noreply.github.com>
2021-03-15 19:37:41 +0100

    Update data_utils.py

--- ttf/utils/data_utils.py
+++ ttf/utils/data_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def load_data_sequences(path1):
-	df = pd.read_csv(path1, sep="\t", header=None, names=["sentence", "id_dep", "human_score"])
-	return df
+    df = pd.read_csv(path1, sep="\t")
+    print(df)
+    return d
