31e9de0cc4b5b957ae845ec0ee934b45b8f92c8a
pedin <pedinotti.paolo@gmail.com>
2021-03-27 18:57:39 +0100

    analysis of errors - to be removed

--- ttf/core/evaluation.py
+++ ttf/core/evaluation.py
@@ -82,7 +82,7 @@ def _correlation(df, output_location, path_data):
 	#----Analysis of errors
 	residuals = {}
 	for n_item in range(len(df)):
-		residuals[df["sentence"][n_item]] = np.abs(probs_predicted[n_item][0] - probs_for_regr[n_item][0])
+		residuals[list(df["sentence"])[n_item]] = np.abs(probs_predicted[n_item][0] - probs_for_regr[n_item][0])
 	residuals = sorted(residuals.items(), key=lambda x: x[1], reverse=True)
 	for item in list(residuals.keys())[0:15]:
 		print("Sentence: {}    Error: {}".format(item, residuals[item]))
