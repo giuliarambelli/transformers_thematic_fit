21531fe065f71d9afb01432dcede3d97d9a84c0f
pedin <pedinotti.paolo@gmail.com>
2021-03-27 12:31:30 +0100

    fix transformers sbj

--- ttf/core/transformers_mlm.py
+++ ttf/core/transformers_mlm.py
@@ -192,7 +192,6 @@ def build_model(path_data, output_directory, transformers, baseline):
     data = pd.read_csv(path_data, sep='\t')
     #thematic_role = os.path.basename(path_data).split("_")[1].split(".")[0]  # funziona solo se lasciamo i nomi dei file con le frasi come sono adesso
     thematic_role = get_thematic_role(list(data.columns), os.path.basename(path_data).split('.')[0])
-    print(thematic_role)
     for transformer in transformers:
         model = TransformerModel(transformer)
         model_fillers_scores, model_completions, baseline_scores = model.compute_fillers_scores(data, thematic_role, BATCH_SIZE)
