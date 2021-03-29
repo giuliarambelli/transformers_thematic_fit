7d00fbf1d197a467e1e433a533c690b8a6543b52
giuliarambelli <rambelligiulia@gmail.com>
2021-03-29 19:50:43 +0200

    coppie comuni

--- ttf/utils/compare_results.py
+++ ttf/utils/compare_results.py
@@ -2,12 +2,14 @@ import os
 import pandas as pd
 from data_utils import get_thematic_role
 
+no_obj = [273,802]
 
 def get_common_indexes(dir_sdm, dir_transf, outdir):
-	files_names = set([f.split('.')[0] for f in os.listdir(dir_sdm) if os.path.isfile(os.path.join(dir_sdm,f))])
+	files_names = sorted(set([f.split('.')[0] for f in os.listdir(dir_sdm) if os.path.isfile(os.path.join(dir_sdm,f))]))
+	res = []
 	# WARNING: it does not deal with subdirectories!
-	for fname in sorted(files_names):
-		print(fname)
+	for fname in files_names:
+		#print(fname)
 		# get files for the same dataset
 		paths = [os.path.join(dir_transf, f) for f in os.listdir(dir_transf) if f.startswith(fname+'.')] + \
 		        [os.path.join(dir_sdm, f) for f in os.listdir(dir_sdm) if  f.startswith(fname+'.')]
@@ -22,6 +24,7 @@ def get_common_indexes(dir_sdm, dir_transf, outdir):
 			# {('spy', 'information', 'pass'): [1, 3], ('volunteer', 'food', 'bring'): [0, 2]}
 			df = pd.read_csv(paths[0], sep='\t')
 			trole = get_thematic_role(df.columns, 'sbj' in fname.lower())
+			if fname.endswith('Triples'): df=df.drop(no_obj)
 			if trole in ['SUBJECT', 'OBJECT']:
 				groups = df.groupby(list(set(['SUBJECT', 'VERB', 'OBJECT']).difference(set([trole])))).groups
 			else:
@@ -29,22 +32,25 @@ def get_common_indexes(dir_sdm, dir_transf, outdir):
 
 			resulting_idxs = []
 			for idx in groups.values():
-				if len(idx)==2:
-					if idx[0] in common_idxs and idx[1] in common_idxs:
-						resulting_idxs.extend(idx)
-				else:
-					if any(j in common_idxs for j in idx[:2]) and any(j in common_idxs for j in idx[2:]):
-				        #if all(j in common_idxs for j in idx):
-						resulting_idxs.extend(idx)
+				m = int(len(idx) / 2)
+				for i, j in zip(idx[:m], idx[m:]):
+					if i in common_idxs and j in common_idxs:
+						resulting_idxs.append((i, j))
+
 			resulting_idxs = sorted(resulting_idxs)
 			#for i in resulting_idxs:
 			#	print(i)
-			print(fname, len(resulting_idxs))
+			print(fname, len(common_idxs), len(resulting_idxs))
+			res.append(resulting_idxs)
+		else:
+			res.append([])
+	df_out = pd.DataFrame(data={'name':files_names, 'pairs': res })
+	df_out.to_csv('dtfit_common_pairs2.txt', sep='\t', index=None)
 
 if __name__ == '__main__':
 	#get_common_indexes('/home/giulia.rambelli/transformers_thematic_fit/datasets/DTFit/sdm_result/',
 	#                   '/home/giulia.rambelli/transformers_thematic_fit/datasets/DTFit/transformers/',
-	#                   '.')
+	#                  '.')
 	get_common_indexes('/home/giulia.rambelli/transformers_thematic_fit/datasets/semantic_plausibility_data/sdm_result/',
-                   '/home/giulia.rambelli/transformers_thematic_fit/datasets/semantic_plausibility_data/transformers/',
-                   '.')
+                  '/home/giulia.rambelli/transformers_thematic_fit/datasets/semantic_plausibility_data/transformers/',
+                  '.')
