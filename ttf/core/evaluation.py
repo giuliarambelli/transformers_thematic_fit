77a5e1952d70e69e638a952fd1e0bb027a7fb927
pedin <pedinotti.paolo@gmail.com>
2021-04-05 17:40:18 +0200

    mean scores sdm

--- ttf/core/evaluation.py
+++ ttf/core/evaluation.py
@@ -61,7 +61,7 @@ def _simple_accuracy(df, selected_pairs, path_data, output_location):
 						print("Error. Sentence typical: {}  Human score: {}   Score assigned: {}".format(df['sentence'][idx[0]], df['mean_rat'][idx[0]], df['computed_score'][idx[0]]))
 						print("Sentence atypical: {}  Human score: {}   Score assigned: {}".format(df['sentence'][idx[1]], df['mean_rat'][idx[1]], df['computed_score'][idx[1]]))
 						print()
-					if (os.path.basename(path_data).startswith("WangDurrettErk")) and (not os.path.basename(path_data).endswith("sdm-res"))::
+					if (os.path.basename(path_data).startswith("WangDurrettErk")) and (not os.path.basename(path_data).endswith("sdm-res")):
 						print("Error. Sentence typical: {}  Score assigned: {}".format(df['sentence'][idx[0]], df['computed_score'][idx[0]]))
 						print("Sentence atypical: {}  Score assigned: {}".format(df['sentence'][idx[1]], df['computed_score'][idx[1]]))
 						print()
@@ -80,8 +80,7 @@ def _simple_accuracy(df, selected_pairs, path_data, output_location):
 						print("Error. Sentence typical: {}  Human score: {}   Score assigned: {}".format(df['sentence'][idx[1]], df['mean_rat'][idx[1]], df['computed_score'][idx[1]]))
 						print("Sentence atypical: {}  Human score: {}   Score assigned: {}".format(df['sentence'][idx[0]], df['mean_rat'][idx[0]], df['computed_score'][idx[0]]))
 						print()
-						if (os.path.basename(path_data).startswith("WangDurrettErk")) and (
-						not os.path.basename(path_data).endswith("sdm-res")): :
+					if (os.path.basename(path_data).startswith("WangDurrettErk")) and (not os.path.basename(path_data).endswith("sdm-res")):
 						print("Error. Sentence typical: {}  Score assigned: {}".format(df['sentence'][idx[1]], df['computed_score'][idx[1]]))
 						print("Sentence atypical: {}  Score assigned: {}".format(df['sentence'][idx[0]], df['computed_score'][idx[0]]))
 						print()
